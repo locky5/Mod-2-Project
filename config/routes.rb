8e8313233eb72adf50fa718c7d7c1d16c72434a9
locky5 <daikevin19@yahoo.com>
2019-06-16 22:56:01 -0400

    Fixed Delete User function

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   resources :games
   resources :languages
 
-  resources :users, only: [:index, :create, :destroy]
+  resources :users, only: [:index, :create]
   get '/profile/:id', to: "users#show", as: "profile"
+  delete '/profile/:id', to: "users#destroy"
   get '/signup', to: "users#new", as: "signup"
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
