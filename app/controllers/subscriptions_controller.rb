f5bcefc971b205066eace4c5a812c1fcb9df1013
Amy Cen <cenamy@gmail.com>
2019-06-17 16:54:08 -0400

    changed redirect for subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -13,7 +13,7 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.new(user_id: params[:subscription][:user_id], channel_id: params[:subscription][:channel_id])
     if @subscription.valid?
       @subscription.save
-      redirect_to profile_path(session[:user_id])
+      redirect_to channel_path(params[:subscription][:channel_id])#profile_path(session[:user_id])
     else
       flash[:message] = 'Unable to subscribe'
       redirect_to channels_path
