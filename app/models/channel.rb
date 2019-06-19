08cfe33d12b541865e9ac8408f95a26028dda296
Amy Cen <cenamy@gmail.com>
2019-06-19 17:43:00 -0400

    handled banned user

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -39,19 +39,18 @@ def self.get_live_streams(api_args: "first=100")
 
       @channel = Channel.find_by(twitch_user_id: twitch_channel["user_id"])
       if !@channel
-        @user_data = RestClient.get "https://api.twitch.tv/helix/users?id=#{twitch_channel["user_id"]}",  { 'Client-ID': "#{@client_id}"}
-        @kraken_user_data = RestClient.get "https://api.twitch.tv/kraken/users/#{twitch_channel["user_id"]}",  { 'Client-ID': "#{@client_id}"}
-        @user_data = JSON.parse(@user_data)
-        @kraken_user_data = JSON.parse(@kraken_user_data)
-        @found_user = @user_data["data"].first["login"]
+          @user_data = RestClient.get "https://api.twitch.tv/helix/users?id=#{twitch_channel["user_id"]}",  { 'Client-ID': "#{@client_id}"}
+          @user_data = JSON.parse(@user_data)["data"]
+          if !@user_data.empty?
+            @found_user = @user_data.first["login"]
 
-        @channel = Channel.new(twitch_user_login: @found_user, twitch_user_id: twitch_channel["user_id"], name: twitch_channel["user_name"], title: twitch_channel["title"], language_id: @language.id, view_count: twitch_channel["viewer_count"], game_id: @game_id, status: twitch_channel["type"], box_art: @final_box_art, logo_url: @user_data["logo"])
-        @channel = Channel.new(twitch_user_login: @found_user, twitch_user_id: twitch_channel["user_id"], name: twitch_channel["user_name"], title: twitch_channel["title"], language_id: @language.id, view_count: twitch_channel["viewer_count"], game_id: @game.id, status: twitch_channel["type"], box_art: @final_box_art, logo_url: @kraken_user_data["logo"])
+            @channel = Channel.new(twitch_user_login: @found_user, twitch_user_id: twitch_channel["user_id"], name: twitch_channel["user_name"], title: twitch_channel["title"], language_id: @language.id, view_count: twitch_channel["viewer_count"], game_id: @game_id, status: twitch_channel["type"], box_art: @final_box_art, logo_url: @found_user["profile_image_url"])
 
-        if @channel.valid?
-          @channel.save
-          curr_live_channels << @channel
-        end
+            if @channel.valid?
+              @channel.save
+              curr_live_channels << @channel
+            end
+          end
       else
         curr_live_channels << @channel
       end
